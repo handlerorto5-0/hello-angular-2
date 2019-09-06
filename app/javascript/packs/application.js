343e0efd3b4e35129a55253f2b5557bda7c40ed9
excavator <str_token@mail.ru>
2019-09-06 12:31:48 +0300

    Hello from Webpack

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,3 +15,5 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+const webpack = "Hello from Webpack !";
+console.log(webpack);
